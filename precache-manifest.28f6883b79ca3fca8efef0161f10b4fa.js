fdb8fe5bb31d6d6c0cec2e57d329cc5438c23a0a
Vishesh Ghorawat <visheshghorawat@gmail.com>
2020-08-19 19:19:02 +0530

    Updates

---
+++ precache-manifest.28f6883b79ca3fca8efef0161f10b4fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73185354694fe08c3ed3ed7ced764bf",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "a82b830c53946c271140",
+    "url": "/deploy-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "414dfd44a336f9f7cc26",
+    "url": "/deploy-react/static/css/main.546c9029.chunk.css"
+  },
+  {
+    "revision": "a82b830c53946c271140",
+    "url": "/deploy-react/static/js/2.d1f19801.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/deploy-react/static/js/2.d1f19801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "414dfd44a336f9f7cc26",
+    "url": "/deploy-react/static/js/main.0e7062ff.chunk.js"
+  },
+  {
+    "revision": "9444bb5009ef0df51c1b",
+    "url": "/deploy-react/static/js/runtime-main.a9f34407.js"
+  },
+  {
+    "revision": "ded112ad0ae1c79554112236955dc5e1",
+    "url": "/deploy-react/static/media/DP.ded112ad.jpeg"
+  }
+]);
